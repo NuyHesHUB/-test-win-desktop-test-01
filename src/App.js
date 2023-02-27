4b981d0ae08a350929f84447bdc52743b281a38a
unknown <otz4193@naver.com>
2023-02-27 17:47:16 +0900

    23.02.27

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+// import Proptypes from 'prop-types';
+import Box from "./Box";
+import Modal from "./Modal";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const ESCAPE_KEY = 27;
+class App extends Component {
+  state = {
+    currentModal: ""
+  };
+  
+  componentDidMount(){
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  toggleModal = modalId => {
+    if (modalId) {
+      this.setState(({ currentModal }) => {
+        return { currentModal: modalId };
+      });
+    } else {
+      this.setState(({ currentModal }) => {
+        return { currentModal: "" };
+      });
+    }
+  };
+
+  handleKeyDown = (event) => {
+      switch( event.keyCode ) {
+          case ESCAPE_KEY:
+              this.toggleModal('');
+              break;
+          default: 
+              break;
+      }
+  }
+
+  render() {
+    const { currentModal } = this.state;
+    return (
+      <div className="body-wrapper">
+        <div
+          className={`container ${currentModal.length > 0 ? "modal-open" : ""}`}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Box
+            title="this is title"
+            cover="https://s3-us-west-2.amazonaws.com/s.cdpn.io/12005/harbour1.jpg"
+            toggleModal={this.toggleModal}
+            modalId="modalOne"
+          >
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+            nostrum aliquid quibusdam hic ut totam dolorum distinctio est quam
+            illo ipsa reiciendis provident placeat, nemo sunt? Laboriosam
+            excepturi aspernatur quo!
+          </Box>
+          <Box
+            title="this is title"
+            cover="https://s3-us-west-2.amazonaws.com/s.cdpn.io/12005/harbour1.jpg"
+            toggleModal={this.toggleModal}
+            modalId="modalTwo"
+          >
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+            nostrum aliquid quibusdam hic ut totam dolorum distinctio est quam
+            illo ipsa reiciendis provident placeat, nemo sunt? Laboriosam
+            excepturi aspernatur quo!
+          </Box>
+          <Box
+            title="this is title"
+            cover="https://s3-us-west-2.amazonaws.com/s.cdpn.io/12005/harbour1.jpg"
+            toggleModal={this.toggleModal}
+            modalId="modalThree"
+          >
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+            nostrum aliquid quibusdam hic ut totam dolorum distinctio est quam
+            illo ipsa reiciendis provident placeat, nemo sunt? Laboriosam
+            excepturi aspernatur quo!
+          </Box>
+        </div>
+        <Modal
+          currentModal={currentModal}
+          toggleModal={this.toggleModal}
+          modalId="modalOne"
+          title="Modal One"
+        >
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          dolor iste amet error natus in quos harum eligendi perspiciatis cum,
+          qui quisquam vero rem perferendis eius, reiciendis assumenda enim
+          repellat.
+        </Modal>
+        <Modal
+          currentModal={currentModal}
+          toggleModal={this.toggleModal}
+          modalId="modalTwo"
+          title="Modal Two"
+        >
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          dolor iste amet error natus in quos harum eligendi perspiciatis cum,
+          qui quisquam vero rem perferendis eius, reiciendis assumenda enim
+          repellat.
+        </Modal>
+        <Modal
+          currentModal={currentModal}
+          toggleModal={this.toggleModal}
+          modalId="modalThree"
+          title="Modal Three"
+        >
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          dolor iste amet error natus in quos harum eligendi perspiciatis cum,
+          qui quisquam vero rem perferendis eius, reiciendis assumenda enim
+          repellat.
+        </Modal>
+      </div>
+    );
+  }
 }
 
-export default App;
+render(<App />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,17 +1,119 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import { render } from "react-dom";
+// import Proptypes from 'prop-types';
+import Box from "./Box";
+import Modal from "./Modal";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const ESCAPE_KEY = 27;
+class App extends Component {
+  state = {
+    currentModal: ""
+  };
+  
+  componentDidMount(){
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  toggleModal = modalId => {
+    if (modalId) {
+      this.setState(({ currentModal }) => {
+        return { currentModal: modalId };
+      });
+    } else {
+      this.setState(({ currentModal }) => {
+        return { currentModal: "" };
+      });
+    }
+  };
+
+  handleKeyDown = (event) => {
+      switch( event.keyCode ) {
+          case ESCAPE_KEY:
+              this.toggleModal('');
+              break;
+          default: 
+              break;
+      }
+  }
+
+  render() {
+    const { currentModal } = this.state;
+    return (
+      <div className="body-wrapper">
+        <div
+          className={`container ${currentModal.length > 0 ? "modal-open" : ""}`}
+        >
+          <Box
+            title="this is title"
+            cover="https://s3-us-west-2.amazonaws.com/s.cdpn.io/12005/harbour1.jpg"
+            toggleModal={this.toggleModal}
+            modalId="modalOne"
+          >
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+            nostrum aliquid quibusdam hic ut totam dolorum distinctio est quam
+            illo ipsa reiciendis provident placeat, nemo sunt? Laboriosam
+            excepturi aspernatur quo!
+          </Box>
+          <Box
+            title="this is title"
+            cover="https://s3-us-west-2.amazonaws.com/s.cdpn.io/12005/harbour1.jpg"
+            toggleModal={this.toggleModal}
+            modalId="modalTwo"
+          >
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+            nostrum aliquid quibusdam hic ut totam dolorum distinctio est quam
+            illo ipsa reiciendis provident placeat, nemo sunt? Laboriosam
+            excepturi aspernatur quo!
+          </Box>
+          <Box
+            title="this is title"
+            cover="https://s3-us-west-2.amazonaws.com/s.cdpn.io/12005/harbour1.jpg"
+            toggleModal={this.toggleModal}
+            modalId="modalThree"
+          >
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+            nostrum aliquid quibusdam hic ut totam dolorum distinctio est quam
+            illo ipsa reiciendis provident placeat, nemo sunt? Laboriosam
+            excepturi aspernatur quo!
+          </Box>
+        </div>
+        <Modal
+          currentModal={currentModal}
+          toggleModal={this.toggleModal}
+          modalId="modalOne"
+          title="Modal One"
+        >
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          dolor iste amet error natus in quos harum eligendi perspiciatis cum,
+          qui quisquam vero rem perferendis eius, reiciendis assumenda enim
+          repellat.
+        </Modal>
+        <Modal
+          currentModal={currentModal}
+          toggleModal={this.toggleModal}
+          modalId="modalTwo"
+          title="Modal Two"
+        >
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          dolor iste amet error natus in quos harum eligendi perspiciatis cum,
+          qui quisquam vero rem perferendis eius, reiciendis assumenda enim
+          repellat.
+        </Modal>
+        <Modal
+          currentModal={currentModal}
+          toggleModal={this.toggleModal}
+          modalId="modalThree"
+          title="Modal Three"
+        >
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          dolor iste amet error natus in quos harum eligendi perspiciatis cum,
+          qui quisquam vero rem perferendis eius, reiciendis assumenda enim
+          repellat.
+        </Modal>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
